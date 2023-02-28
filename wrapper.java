70d7e87750f1e902eeef8da130d72b06703f97b5
david <davidjhoyjr@gmail.com>
2023-02-28 14:53:49 -0500

    first

---
+++ wrapper.java
@@ -0,0 +1,9 @@
+class wrapper {
+    public static void main(String[] args ){
+        System.out.println("Integer Max Val:" + Integer.MAX_VALUE);
+        System.out.println("Short Max Val:" + Short.MAX_VALUE);
+        System.out.println("Long Max Val:" + Long.MAX_VALUE);
+        System.out.println("Double Max Val:" + Double.MAX_VALUE);
+        
+    }
+}
